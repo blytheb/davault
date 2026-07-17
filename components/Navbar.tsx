47f7e8e1e89af5ac7ec0d129a0d34bc57dc36df2
blytheb <blytheballesteros@gmail.com>
2026-07-17 10:24:17 -1000

    style nav bar: seperated auth buttons from page routes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,11 +7,17 @@ import {
 
 export default function Navbar() {
 	return (
-		<nav className="space-x-5 border-b border-gray-400">
-			<Link href={"/"}>Landing</Link>
-			<Link href={"/sign-up"}>Sign Up</Link>
-			<Link href={"/login"}>Log In</Link>
-			<Link href={"/dashboard"}>Dashboard</Link>
+		<nav className="flex justify-between items-center border-b border-gray-400">
+			<div className="space-x-5">
+				<Link href={"/"}>Landing</Link>
+				<Link href={"/dashboard"}>Dashboard</Link>
+				<Link href={"/vaults"}>Vaults</Link>
+				<Link href={"/trasnactions"}>Transactions</Link>
+			</div>
+			<div className="space-x-5">
+				<Link href={"/sign-up"}>Sign Up</Link>
+				<Link href={"/login"}>Log In</Link>
+			</div>
 		</nav>
 	);
 }
