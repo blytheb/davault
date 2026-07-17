a4945bcbfedd72fd6a8f0df41b974d62fd120d93
blytheb <blytheballesteros@gmail.com>
2026-07-17 13:57:34 -1000

    test: added user info on dashboard to test log in

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+
 export default function Dashboard() {
-	return <div>Dashboard</div>;
+	const { data: session, isPending } = authClient.useSession();
+
+	console.log(session);
+
+	if (isPending) return <p>Loading...</p>;
+
+	return <pre>{JSON.stringify(session, null, 2)}</pre>;
 }
