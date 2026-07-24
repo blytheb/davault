9bde9c95c874fed9e5e889a2196044fab4b4accf
blytheb <blytheballesteros@gmail.com>
2026-07-23 15:59:26 -1000

    added progress bar for vault card

--- app/(public)/vaults/page.tsx
+++ app/(public)/vaults/page.tsx
@@ -1,16 +1,19 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
 import React from "react";
 
 export default function Vaults() {
 	return (
-		<div>
+		<>
 			Vaultspage
-			<div className="space-y-5 sm:flex sm:space-x-5 sm:space-y-0 items-center justify-center">
+			<div className="space-y-5 sm:flex sm:space-x-5 sm:space-y-0 items-center justify-center  m-4">
 				<Card className="w-full sm:w-[250px]">
 					<CardHeader>Vault #1</CardHeader>
 					<CardContent>
 						<div>
 							<h1>$1000</h1>
+							<Progress value={80} />
+							<p className="text-right">80% Completed</p>
 						</div>
 					</CardContent>
 				</Card>
@@ -19,6 +22,8 @@ export default function Vaults() {
 					<CardContent>
 						<div>
 							<h1>$1000</h1>
+							<Progress value={40} />
+							<p className="text-right">40% Completed</p>
 						</div>
 					</CardContent>
 				</Card>
@@ -27,6 +32,8 @@ export default function Vaults() {
 					<CardContent>
 						<div>
 							<h1>$1000</h1>
+							<Progress value={60} />
+							<p className="text-right">60% Completed</p>
 						</div>
 					</CardContent>
 				</Card>
@@ -35,10 +42,12 @@ export default function Vaults() {
 					<CardContent>
 						<div>
 							<h1>$1000</h1>
+							<Progress value={10} />
+							<p className="text-right">10% Completed</p>
 						</div>
 					</CardContent>
 				</Card>
 			</div>
-		</div>
+		</>
 	);
 }
